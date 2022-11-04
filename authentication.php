41f13ddcc90033745dc7f2f537e40cfb51cda01d
Deepanshu Dheer <103347008+deepanshudheer@users.noreply.github.com>
2022-11-04 16:26:37 +0530

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -1,28 +1,26 @@
-<?php      
-    include('connection.php');  
-    $username = $_POST['username'];  
-    $password = $_POST['password'];  
-      
-        //to prevent from mysqli injection  
-        $username = stripcslashes($username);  
-        $password = stripcslashes($password);  
-        $username = mysqli_real_escape_string($con, $username);  
-        $password = mysqli_real_escape_string($con, $password);  
-      
-        $sql = "select *from login where username = '$username' and password = '$password'";  
-        $result = mysqli_query($con, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);  
-          
-        if($count == 1){  
-            echo "<h1><center> Login successful </center></h1>";  
-            header("Location: landing.html"); 
-            exit;
-        }  
-        else{  
-            echo '<script type ="text/JavaScript">';  
-            echo 'alert("JavaScript Alert Box by PHP")';  
-            echo '</script>';  
-            header("Location: index.html");
-        }     
+<?php
+include('connection.php');
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//to prevent from mysqli injection  
+$username = stripcslashes($username);
+$password = stripcslashes($password);
+$username = mysqli_real_escape_string($con, $username);
+$password = mysqli_real_escape_string($con, $password);
+
+$sql = "select *from login where username = '$username' and password = '$password'";
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
+
+if ($count == 1) {
+    echo "<h1><center> Login successful </center></h1>";
+    header("Location: home.html");
+    exit;
+} else {
+    header("Location: index.html?validationsuccess=no");
+    // echo "<script type='text/javascript'></script>";
+    sleep(1);
+}
 ?>
