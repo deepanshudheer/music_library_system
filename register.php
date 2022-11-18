825c00194c57fd38c3a8966c61154ffe3c0c8a2d
Deepanshu Dheer <103347008+deepanshudheer@users.noreply.github.com>
2022-11-18 10:36:57 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,22 +1,28 @@
 <?php
-include('connection.php');
+require_once('config.php');
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 
 //to prevent from mysqli injection  
 $username = stripcslashes($username);
 $password = stripcslashes($password);
-$username = mysqli_real_escape_string($con, $username);
-$password = mysqli_real_escape_string($con, $password);
+$username = mysqli_real_escape_string($conn, $username);
+$password = mysqli_real_escape_string($conn, $password);
 
+$sql = "select * from login where username='$username'";
+$res_login = mysqli_query($conn, $sql);
+$sql_num = mysqli_num_rows($res_login);
+if ($sql_num > 0) {
+    echo "Username already exists! Please try with another username";
+} else {
 
-if (isset($_POST['username']) && ($_POST['password'])) {
-    $sql = "insert into login (login_id, username, password) values ('', '$username', '$password')";
+    if (isset($_POST['username']) && ($_POST['password'])) {
+        $sql = "insert into login (login_id, username, password) values ('', '$username', '$password')";
+    }
 }
-
-if ($con->query($sql) === TRUE) {
-    header("Location: home.html");
+if ($conn->query($sql) === TRUE) {
+    header("Location: home.php");
     exit;
 } else {
-    echo "Error: Couldn't register" .mysqli_error($con);
+    header("Location: registerpage.php?registersuccess=no");
 }
