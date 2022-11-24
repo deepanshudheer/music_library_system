d80cfe9e14884b6be9b6076c60aa74fc1d40fad5
Deepanshu Dheer <103347008+deepanshudheer@users.noreply.github.com>
2022-11-24 11:50:52 +0530

    Update session.php

--- session.php
+++ session.php
@@ -1,9 +1,9 @@
 <?php
-include('connection.php');
+require_once('config.php');
 session_start();
 
 $user_check=$_SESSION['login_user'];
-$ses_sql=mysqli_query($con,"select username, login_id from login where username='$user_check'");
+$ses_sql=mysqli_query($conn,"select username, login_id from login where username='$user_check'");
 $row=mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
 $loggedin_session=$row['username'];
 $loggedin_id=$row['login_id'];
